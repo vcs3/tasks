c082d50210c610c9a31fc5d5544a451cc3b81a9a
vcs3 <vcs3@cin.ufpe.br>
2015-08-10 16:58:52 -0300

    dados iniciais

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,4 +1,5 @@
 import tasks.Category
+import tasks.Task
 
 class BootStrap {
 
@@ -15,6 +16,44 @@ class BootStrap {
 				if(pro.hasErrors()){
 				println pro.errors
 				}
+		 def task1 = new Task(
+			    task:"Devolver livros emprestados",
+				requiredBy:(new Date() +10),
+				complete:true,
+				category:pes
+			 	
+			 )
+		 task1.save()
+		 if(task1.hasErrors()){
+			 println task1.errors
+			 }
+		 def task2 = new Task(
+			 task:"Apresentar projeto para os clientes",
+			 requiredBy:(new Date()),
+			 complete:false,
+			 category:pro
+			  
+		  )
+	  task2.save()
+	  if(task2.hasErrors()){
+		  println task2.errors
+		  }
+	  
+	  def task3 = new Task(
+		  task:"Encontrar o pessoal no happy hour",
+		  requiredBy:(new Date() +2),
+		  complete:false,
+		  category:pes
+		   
+	   )
+		   task3.save()
+		   if(task3.hasErrors()){
+			   println task3.errors
+			   }
+	  
+	  
+	  
+		 
     }
     def destroy = {
     }
